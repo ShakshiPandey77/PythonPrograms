ce7ce2887e800863de81033804766491a2de10dc
ShakshiPandey77 <49754283+ShakshiPandey77@users.noreply.github.com>
2019-10-14 18:18:42 +0530

    Update password.py

--- password.py
+++ password.py
@@ -5,4 +5,34 @@ print(str1)
 print(len(str1))
 for i in range(6):
     print(str1[randint(0,100)],end="")
+    
+    
+    
+    
+    
+    ''' random passsord generator program.
+    Constraints : a. Minimum length of 10, maximum length of 15
+                  b. Password must contain at least one capital letter,
+                     one small letter, one number and one special character
+    '''
+    
+    import string
+from random import randint
+str1 = string.printable
+
+def generatePassword():
+    passLen = randint(10,15)
+    password = ''
+    for i in range(passLen):
+        password+=str1[randint(0,len(str1)-1)]
+    return password
+
+while True:
+    password =  generatePassword()
+    if (any(x.isupper() for x in password) and
+       any(x.islower() for x in password) and
+       any(x.isdigit() for x in password) and
+       (len(password)>=10 or len(password)<=15) ):
+           print(password)
+           break
 
