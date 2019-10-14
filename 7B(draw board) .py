4e224568788ce9022712ec0658587bb9cd3716cb
ShakshiPandey77 <49754283+ShakshiPandey77@users.noreply.github.com>
2019-10-14 18:30:16 +0530

    Create 7B(draw board) .py

---
+++ 7B(draw board) .py
@@ -0,0 +1,10 @@
+import sys
+for i in range(3):
+	for j in range(3):
+		print("--- ",end="")
+	print()
+	for k in range(4):
+		print("|  ",end="")
+	print()
+for m in range(3):
+		print("--- ",end="")
